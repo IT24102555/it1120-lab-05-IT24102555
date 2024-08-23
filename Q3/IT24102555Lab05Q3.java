472d50b5634fa987750558cc36627360a140df56
IT24102555 <it24102555@my.sliit.lk>
2024-08-23 22:27:41 +0530

    Update IT24102555Lab5Q3.java

--- Q3/IT24102555Lab05Q3.java
+++ Q3/IT24102555Lab05Q3.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
   public class  IT24102555Lab05Q3 {
      public static void main(String[] args) {
-	 Scanner input = new Scanner(System.in);
-
+	
          final double Roomchargesperdays= 48000.00;
          final double Three_or_four_days_discount = 10/100;
          final double five_or_more_days_discount = 20/100;
-            
-	 System.out.print("Enter the start date(1-31):");
+
+	 Scanner input = new Scanner(System.in);
+         System.out.print("Enter the start date(1-31):");
 	 int startdate = input.nextInt();
 
          System.out.print("Enter the end date(1-31):");
@@ -20,14 +20,13 @@ if(startdate>enddate)
 
         int NoOfDaysReserved = enddate-startdate;
  
- double totalAmount = NoOfDaysReserved * Roomchargesperdays;
+       double totalAmount = NoOfDaysReserved * Roomchargesperdays;
+	     
 if(NoOfDaysReserved>=3 && NoOfDaysReserved<=4)
       discountAmount=totalAmount*Three_or_four_days_discount;
 if(NoOfDaysReserved>=5)
       discountAmount=totalAmount*five_or_more_days_discount;
    double finalAmount =totalAmount-discountAmount;
-  
-
   
       System.out.print("Number Of Days Reserved:"+NoOfDaysReserved);
       System.out.print("Total amount to be paid:" +finalAmount);
